32296294ae1f76d580507c62675367c47e91ad24
admins <ufkz200147@gmail.com>
2023-09-20 22:11:06 +0600

    Инициализировала фронт, убрала лишние файлы

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {CssBaseline} from "@mui/material";
+import {Routes} from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <header></header>
+      <main>
+        <Routes>
+
+        </Routes>
+      </main>
+    </>
+  );
+};
+
+export default App;
